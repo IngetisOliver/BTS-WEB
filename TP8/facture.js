46105c5794d8650508444d72099ab317a499c90c
IngetisOliver <144143292+IngetisOliver@users.noreply.github.com>
2023-10-24 14:51:22 +0200

    Add files via upload

---
+++ TP8/facture.js
@@ -0,0 +1,14 @@
+function multiplication(){
+    r1.value = Number(c2.value)*Number(c3.value);
+    r2.value = Number(c4.value)*Number(c5.value);
+    r3.value = Number(c6.value)*Number(c7.value);
+    R4.value = Number(r1.value)+Number(r2.value)+Number(r3.value);
+
+    /* t3.value = Number(t1.value)*Number(t2.value); */
+
+}
+
+function reset(){
+    location.reload()
+}
+
